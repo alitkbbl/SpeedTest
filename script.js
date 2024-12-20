6566ca0c5cc04c830b14a373f99038883ac4d80e
Ali <alitaghizadeh8305@gmail.com>
2024-12-20 23:40:43 +0330

    my commit in this proje

--- script.js
+++ script.js
@@ -1,25 +1,72 @@
-const theTimer=document.querySelector(".timer");
+const timerSet = document.querySelector(".timer");
+const testArea = document.querySelector("#test-area");
+const originTxt = document.querySelector("#origin-text p").innerHTML;
+const testWrapper = document.querySelector(".test-wrapper");
+const restart = document.querySelector("#reset");
 
+ var timer=[0,0,0];
+ var s=1;
+ let timerTime;
 
-var timer=[0,0,0,0];
-
-
-
-
-function runTimer(){
-let currentTime= timer[0]+":"+timer[1]+":"+timer[2];
-
-theTimer.innerHTML=currentTime;
-
-timer[3]++;
-
-timer[0]=Math.floor((timer[3]/100)/60);
-timer[1]=Math.floor(timer[3]/100)-(timer[0]*60);
-timer[2]=Math.floor(timer[3] - (timer[1]*100) - (timer[0]*6000));
-
-
-
+function creatcheck(timer) {
+    
+    if(timer<=9) timer='0'+timer;
+    return timer;
 }
 
+ function setTimerShow() {
 
-setInterval(runTimer,10);
+    let craete = creatcheck(timer[0]) + ":" + creatcheck(timer[1]) + ":"+creatcheck(timer[2])%100;
+    timer[2]++;
+    timer[0]= Math.floor(timer[2]/100/60);
+    timer[1]= Math.floor(timer[2]/100-timer[0]*60);
+
+    timerSet.innerHTML = craete;
+ }
+
+
+ function startProsess() {
+    if(s==1){
+       timerTime = setInterval(setTimerShow,10);
+        s=0;
+    }
+ }
+ 
+
+ function spell() {
+
+    let textEntered=testArea.value;
+    let originTextTMP = originTxt.substring(0,textEntered.length);
+    
+
+
+    if(textEntered == originTxt){
+        testWrapper.style.borderColor="green";
+        clearInterval(timerTime);
+    }
+    else{
+        if(textEntered == originTextTMP){
+            testWrapper.style.borderColor="yellow";
+        }
+        else{
+            testWrapper.style.borderColor="red";
+        }
+    }
+    
+ }
+
+function reset() {
+
+    clearInterval(timerTime);
+    timer=[0,0,0];
+    s=1;
+    testArea.value="";
+    timerSet.innerHTML="00:00:00";
+    testWrapper.style.borderColor="grey";
+}
+
+ testArea.addEventListener("keypress",startProsess);
+ testArea.addEventListener("keyup",spell);
+ restart.addEventListener("click",reset)
+
+
